dd64a583b7835c7f48149015958588835d2cdbe4
Fadel <59105868+SrWither@users.noreply.github.com>
2024-04-28 19:19:52 -0300

    discord oauth2 added

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -12,6 +12,7 @@ import { AuthStore } from '@/stores/auth'
 
 import { googleAuth } from '@/api/oauth2/google'
 import { microsoftSignIn } from '@/api/oauth2/microsoft'
+import { exchangeCodeForToken, getCode, getUserData } from '@/api/oauth2/discord'
 
 // Reactive references for user input
 const email = ref<string>('')
@@ -56,6 +57,21 @@ const handleMicrosoft = async () => {
     email: data.preferred_username
   })
 }
+
+// Handler for Discord OAuth login
+const handleDiscord = async () => {
+  const code = await getCode()
+  console.log(code)
+  const access_token = await exchangeCodeForToken(code)
+  console.log(access_token)
+  const userData = await getUserData(access_token)
+  console.log(userData)
+
+  await handleOAuthData({
+    sub: userData.id,
+    email: userData.email
+  })
+}
 </script>
 
 <template>
@@ -78,7 +94,7 @@ const handleMicrosoft = async () => {
           >
         </div>
 
-        <div class="flex items-center justify-between mb-6 space-x-4">
+        <div class="grid grid-cols-2 gap-3 mb-6">
           <button
             @click="handleGoogle"
             class="w-full bg-red-600 text-white hover:bg-red-700 rounded-full py-2 px-4 flex items-center justify-center"
@@ -88,7 +104,7 @@ const handleMicrosoft = async () => {
           </button>
           <button
             @click="handleMicrosoft"
-            class="w-full bg-blue-600 text-white hover:bg-blue-700 rounded-full py-2 px-4 flex items-center justify-center"
+            class="w-full bg-sky-600 text-white hover:bg-sky-700 rounded-full py-2 px-4 flex items-center justify-center"
           >
             <span class="mr-2">Microsoft</span>
             <i class="pi pi-microsoft"></i>
@@ -98,6 +114,13 @@ const handleMicrosoft = async () => {
           >
             <span class="mr-2">GitHub</span>
             <i class="pi pi-github"></i>
+          </button>
+          <button
+            @click="handleDiscord"
+            class="w-full bg-blue-600 text-white hover:bg-blue-700 rounded-full py-2 px-4 flex items-center justify-center"
+          >
+            <span class="mr-2">Discord</span>
+            <i class="pi pi-discord"></i>
           </button>
         </div>
 
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -13,6 +13,7 @@ import { AuthStore } from '@/stores/auth'
 
 import { googleAuth } from '@/api/oauth2/google'
 import { microsoftSignIn } from '@/api/oauth2/microsoft'
+import { getCode, exchangeCodeForToken, getUserData } from '@/api/oauth2/discord'
 
 // Reactive references for user input
 const email = ref<string>('')
@@ -67,6 +68,27 @@ const handleMicrosoft = async () => {
     'microsoft'
   )
 }
+
+// Handler for Discord OAuth registration
+const handleDiscord = async () => {
+  const code = await getCode()
+  console.log(code)
+  const access_token = await exchangeCodeForToken(code)
+  console.log(access_token)
+  const userData = await getUserData(access_token)
+  console.log(userData)
+
+  const avatar = `https://cdn.discordapp.com/avatars/${userData.id}/${userData.avatar}.png`
+  await handleOAuthData(
+    {
+      sub: userData.id,
+      email: userData.email,
+      name: userData.global_name,
+      picture: avatar
+    },
+    'discord'
+  )
+}
 </script>
 
 <template>
@@ -79,7 +101,7 @@ const handleMicrosoft = async () => {
       <!-- Login form -->
       <OSimpleCard class="lg:w-full lg:max-w-md w-96">
         <!--Title-->
-        <div class="flex justify-center mb-6">
+        <div class="flex justify-center mb-4">
           <h1 class="text-gray-800 dark:text-white text-3xl font-bold">Register</h1>
         </div>
 
@@ -89,7 +111,7 @@ const handleMicrosoft = async () => {
           >
         </div>
 
-        <div class="flex items-center justify-between mb-6 space-x-4">
+        <div class="grid grid-cols-2 gap-3 mb-6">
           <button
             @click="handleGoogle"
             class="w-full bg-red-600 text-white hover:bg-red-700 rounded-full py-2 px-4 flex items-center justify-center"
@@ -99,7 +121,7 @@ const handleMicrosoft = async () => {
           </button>
           <button
             @click="handleMicrosoft"
-            class="w-full bg-blue-600 text-white hover:bg-blue-700 rounded-full py-2 px-4 flex items-center justify-center"
+            class="w-full bg-sky-600 text-white hover:bg-sky-700 rounded-full py-2 px-4 flex items-center justify-center"
           >
             <span class="mr-2">Microsoft</span>
             <i class="pi pi-microsoft"></i>
@@ -109,6 +131,13 @@ const handleMicrosoft = async () => {
           >
             <span class="mr-2">GitHub</span>
             <i class="pi pi-github"></i>
+          </button>
+          <button
+            @click="handleDiscord"
+            class="w-full bg-blue-600 text-white hover:bg-blue-700 rounded-full py-2 px-4 flex items-center justify-center"
+          >
+            <span class="mr-2">Discord</span>
+            <i class="pi pi-discord"></i>
           </button>
         </div>
 
